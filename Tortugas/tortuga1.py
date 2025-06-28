59ae91f4d5020dc109c5bcddd3fd0082e054e7ac
Leonardo223-Mtz <leonardo.martinez223@gmail.com>
2025-06-28 09:22:11 -0600

    turtles

---
+++ Tortugas/tortuga1.py
@@ -0,0 +1,23 @@
+import turtle
+import time
+
+turtle.forward(100)
+time.sleep(3)
+turtle.right(90)
+time.sleep(3)
+turtle.backward(50)
+time.sleep(3)
+turtle.left(90)
+time.sleep(3)
+turtle.circle(150)
+
+turtle.penup()
+turtle.setx(-100)
+turtle.sety(-100)
+turtle.pendown()
+turtle.circle(100)
+
+
+
+
+
